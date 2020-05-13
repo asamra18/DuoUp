2a9ff887048d272e53c8a567b689386017630a46
asamra18 <anandsamra@hotmail.com>
2020-05-12 19:25:18 -0700

    Created basic user interface fucntionality.

---
+++ app/src/main/java/com/example/anandsamra/duoup/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.anandsamra.duoup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// RIOT API KEY: RGAPI-4ff39641-8091-452e-b372-28f63b357652
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/anandsamra/duoup/MyProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.anandsamra.duoup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// TODO: Add API usages for summoner name and history and shit.
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
